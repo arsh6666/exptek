999a0b878fb5e147feb9f5afe83e2a67d17f99b8
Arshdeep Singh <arshdeep@CTGARSHDEEP>
2023-02-22 19:53:54 +0400

    created pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutUsComponent } from './components/pages/about-us/about-us.component';
+import { ContactUsComponent } from './components/pages/contact-us/contact-us.component';
+import { ServicesComponent } from './components/pages/services/services.component';
+import { ServiceDetailComponent } from './components/pages/service-detail/service-detail.component';
+import { BlogDetailComponent } from './components/pages/blog-detail/blog-detail.component';
+import { BlogsComponent } from './components/pages/blogs/blogs.component';
+import { PrivacyPolicyComponent } from './components/pages/privacy-policy/privacy-policy.component';
+import { TermConditionsComponent } from './components/pages/term-conditions/term-conditions.component';
+import { NavbarComponent } from './components/common/navbar/navbar.component';
+import { FooterComponent } from './components/common/footer/footer.component';
+import { HomeServicesComponent } from './components/common/home-services/home-services.component';
+import { HomeProductsComponent } from './components/common/home-products/home-products.component';
+import { TestimonialsComponent } from './components/common/testimonials/testimonials.component';
+import { HomeSliderComponent } from './components/common/home-slider/home-slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    ServicesComponent,
+    ServiceDetailComponent,
+    BlogDetailComponent,
+    BlogsComponent,
+    PrivacyPolicyComponent,
+    TermConditionsComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeServicesComponent,
+    HomeProductsComponent,
+    TestimonialsComponent,
+    HomeSliderComponent
   ],
   imports: [
     BrowserModule,
